9a35c91699b96a8c69abb29342c54b771db6521d
vendasdropk9-gif <vendasdropk9@gmail.com>
2026-08-16 10:59:45 -0300

    Update alias path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,9 @@ export default defineConfig(({mode}) => {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
+       {
+  '@': path.resolve(__dirname, 'src')
+}
       },
     },
     server: {
